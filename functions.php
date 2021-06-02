fbad19e842b9dd1411636307d3efc12b4c1aa451
mbadilla <mario.badilla@gmail.com>
2021-06-02 11:59:12 -0600

    added language CR

--- functions.php
+++ functions.php
@@ -7,3 +7,11 @@
 add_action( 'after_setup_theme', function () {
 	// TODO
 } );
+
+/**
+ * Loads the child theme textdomain.
+ */
+function ticknor_child_theme_setup() {
+    load_child_theme_textdomain( 'pressbooks-book', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'ticknor_child_theme_setup' );
